4fb8eccbdb70fa1b2e16e1aa16e2a5166153d4e6
Antonio Clavijo Cortes <littletonito@gmail.com>
2020-09-14 23:39:43 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -14,7 +14,7 @@ def countdown_with_sleep(number)
   number = 10
   while number > 0
     puts "#{number} SECOND(S)!"
-    sleep(1. seconds)
+    sleep 1 
     number -= 1
   end
   "HAPPY NEW YEAR!"
